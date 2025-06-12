4e6fe3779b66732213fa468226517fc5f58de8b6
Juan Vallejo <bagy45@gmail.com>
2025-06-11 22:08:50 -0400

    Refactor activity stats rendering and improve data fetching logic in script.js

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 const dailyTab = document.getElementById('daily')
 const weeklyTab = document.getElementById('weekly')
 const monthlyTab = document.getElementById('monthly')
-let activityContent = document.getElementById('activity')
-let hours = activityContent.querySelector('h2')
-let previous = activityContent.querySelector('p') 
 
-
-
-
-async function getContent() {
-    try {
-        let dailyContent = (await fetch('data.json'))
-        .then
-    }
-    
-
+const cards = {
+    Work: document.getElementById('Work'),
+    Play: document.getElementById('Play'),
+    Study: document.getElementById('Study'),
+    Exercise: document.getElementById('Exercise'),
+    Social: document.getElementById('Social'),
+    'Self Care': document.getElementById('Self Care')
 }
+
+
+dailyTab.addEventListener('click', () => {
+    updateActivityContent('daily');
+});
+weeklyTab.addEventListener('click', () => {
+    updateActivityContent('weekly');
+});
+monthlyTab.addEventListener('click', () => {
+    updateActivityContent('monthly');
+});
+
+let activityData = []
+
+async function getData() {
+    try {
+        const response = await fetch('data.json');
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+        activityData = await response.json();
+        updateActivityContent('daily');
+        console.log(data);
+    }catch (error) {
+        console.error('Error fetching data', error)
+    }
+}
+
+function updateActivityContent(timeframe) {
+    activityData.forEach(activity => {
+        const card = cards[activity.title];
+        if (card) {
+            const hours = card.querySelector('h3');
+            const previous = card.querySelector('p');
+            hours.textContent = `${activity.timeframes[timeframe].current}hrs`;
+            previous.textContent = `Last ${getLastLabel(timeframe)} - ${activity.timeframes[timeframe].previous}hrs`;
+        }
+    });
+}
+
+function getLastLabel(timeframe) {
+    switch (timeframe) {
+        case 'daily': return 'Day';
+        case 'weekly': return 'Week';
+        case 'monthly': return 'Month';
+        default: return '';
+    }
+}
+getData();
